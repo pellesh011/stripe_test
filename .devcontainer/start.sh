c090e92688949208ff850b29269be6d15ebd2aab
Pellesh011 <pellesh011@gmail.com>
2026-08-14 17:32:38 +0300

    chore: added envs to start.sh

--- .devcontainer/start.sh
+++ .devcontainer/start.sh
@@ -2,17 +2,39 @@
 
 set -e
 
+echo "Creating backend .env..."
+
+cat > .env <<EOF
+# Подключение к БД (основная)
+DATABASE_HOST=127.0.0.1
+DATABASE_PORT=5433
+DATABASE_USER=${DATABASE_USER}
+DATABASE_PASSWORD=${DATABASE_PASSWORD}
+DATABASE_DB=${DATABASE_DB}
+
+# Подключение к БД для тестов
+TEST_DATABASE_HOST=127.0.0.1
+TEST_DATABASE_PORT=5434
+TEST_DATABASE_USER=${DATABASE_USER}
+TEST_DATABASE_PASSWORD=${DATABASE_PASSWORD}
+TEST_DATABASE_DB=stripe
+
+# Ключи Stripe (тестовые)
+STRIPE_SECRET_KEY=${STRIPE_SECRET_KEY}
+STRIPE_WEBHOOK_SECRET=${STRIPE_WEBHOOK_SECRET}
+STRIPE_PUBLIC_KEY=${STRIPE_PUBLIC_KEY}
+EOF
+
+echo "Creating frontend/.env..."
+
+cat > frontend/.env <<EOF
+VITE_STRIPE_PUBLIC_KEY=${STRIPE_PUBLIC_KEY}
+EOF
+
 echo "Starting Docker Compose..."
 
 docker compose up -d
 
-echo "Waiting for PostgreSQL..."
+echo "Docker Compose started."
 
-until docker compose exec -T db pg_isready \
-    -U "${DATABASE_USER}" \
-    -d "${DATABASE_DB}"
-do
-    sleep 2
-done
-
-echo "PostgreSQL is ready."
+docker compose ps
